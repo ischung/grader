464af93fc0a7ea88b45dc92b3a39e2ce9a0d6198
Insang Chung <insang@hansung.ac.kr>
2016-02-16 10:24:41 +0900

    POM.xml파일 수정 및 main 클래스 추가

--- src/main/java/calculateGrade/Main.java
+++ src/main/java/calculateGrade/Main.java
@@ -3,7 +3,7 @@ package calculateGrade;
 public class Main {
 
 	public static void main(String[] args) {
-		Scores scores = new Scores(new SimpleScoreStrategy());
+		    Scores scores = new Scores(new SimpleScoreStrategy());
 	    Score score = new Score(35, 30, 25);
 	    String sid = "1234";
 	    scores.addScore(sid, score);
